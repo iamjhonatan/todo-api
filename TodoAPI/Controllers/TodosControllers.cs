eee94c1f9c1a937d0db49b0cf49b5e6dd8a30b0b
Jhonatan Medeiros <jhonatanmedeiros7@gmail.com>
2023-01-31 21:28:24 -0300

    class 59: creating a minimalAPI using dapper and sql server- IV

--- TodoAPI/Controllers/TodosControllers.cs
+++ TodoAPI/Controllers/TodosControllers.cs
@@ -16,7 +16,43 @@ public static class TodosControllers
 
             return Results.Ok(todos);
         });
-        
-        
+
+        app.MapGet("/todos/{id}", async (TodoContext.GetConnection connectionGetter, int id) =>
+        {
+            using var con = await connectionGetter();
+            var todo = con.Get<Todo>(id);
+
+            if (todo is null) return Results.NotFound();
+
+            return Results.Ok(todo);
+        });
+
+        app.MapPost("/todos", async (TodoContext.GetConnection connectionGetter, Todo todo) =>
+        {
+            using var con = await connectionGetter();
+            var id = con.Insert(todo);
+
+            return Results.Created($"/todos/{id}", todo);
+        });
+
+        app.MapPut("/todos", async (TodoContext.GetConnection connectionGetter, Todo todo) =>
+        {
+            using var con = await connectionGetter();
+            var id = await con.UpdateAsync(todo);
+
+            return Results.Ok();
+        });
+
+        app.MapDelete("/todos/{id}", async (TodoContext.GetConnection connectionGetter, int id) =>
+        {
+            using var con = await connectionGetter();
+            var todo = con.Get<Todo>(id);
+
+            if (todo is null) return Results.NotFound();
+
+            await con.DeleteAsync(todo);
+
+            return Results.Ok(todo);
+        });
     }
 }
